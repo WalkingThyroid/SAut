ec19dcf52821a72ea898f0213a54e68aac2825f0
WalkingThyroid <56550454+WalkingThyroid@users.noreply.github.com>
2019-11-22 19:24:13 +0000

    Add files via upload

--- occ_grid_node.py
+++ occ_grid_node.py
@@ -23,31 +23,32 @@ class grid_map(object):
 
 		self.occ_pub = rospy.Publisher("/mbot/occMap", OccupancyGrid, queue_size = 10)
 	
-		self.height = 10 # metric (should probably be in cells
-		self.width = 10 # same as height
-		self.res = 4 # it's in cell/m, should probably be in m/cell
+		self.height = 72	# cells
+		self.width = 72		# cells
+		self.res = 0.25		# m/cell
 		self.pose = [0,0,0]
 		
-		self.alpha = 1
+		self.alpha = 0.5
 		self.beta = 5*np.pi/360 # 5 degrees
-		self.z_max = 10  # initialized on update
+		self.z_max = 10  	# initialized on update
 		
 		self.l_occ = np.log(0.7/0.3)
 		self.l_free = np.log(0.3/0.7)
 		self.l0 = np.log(0.5/0.5)
 	
 		# initialize grid
-		self.grid = np.ndarray( (self.height*self.res,self.width*self.res), dtype=float)
-		self.grid.fill(0.5) # -1 is unkown
+		self.grid = np.ndarray( (self.height,self.width), dtype=float)
+		self.grid.fill(0.5) # unkown (is it right?)
 		
-		'''self.grid_x = np.ndarray( (self.height*self.res,self.width*self.res), dtype=float) # deepcopy?
+		'''self.grid_x = np.ndarray( (self.height,self.width), dtype=float) # deepcopy?
 		it = np.nditer(self.grid_x, flags=['multi_index'])
 		while not it.finished:
-			self.grid_x(it.multi_index(0), it.multi_index(1)) = it.multi_index(0)*(1/self.res) + 1/(self.res * 2)
+			self.grid_x(it.multi_index(0), it.multi_index(1)) = it.multi_index(0)*self.res + self.res * 2
 			it.iternext()''' # maybe a pre-calculated matrix with coordinates
 
 		
 	def update_pose(self, msg):
+		'''Updates the robot pose after receiving an odometry message'''
 		
 		self.pose[0] = msg.pose.pose.position.x
 		self.pose[1] = msg.pose.pose.position.y
@@ -57,29 +58,30 @@ class grid_map(object):
 		self.pose[2] = yaw
 
 	def update(self, msg):
+		'''Update the map after receiving message with measurements'''
 
 		self.z_max = msg.range_max
 		it = np.nditer(self.grid, flags=['multi_index'])
 		while not it.finished:
 			self.grid[it.multi_index] += self.inv_model(it.multi_index, msg)
 			it.iternext()
+		if it.finished:
+			print("Finished one iteration")
 		self.publish_map()
-		self.print_map_to_file()
+		#self.print_map_to_file()
 			
 	def inv_model(self, cell, msg):
+		'''Implementation of inverse model of Probabilistic Robotics'''
 
 		pose = self.world_to_grid(self.pose)
-		print("Pose: " + str(pose))
 		cell_pos = self.get_cell_pos(cell)
-		print("Cell position: " + str(cell_pos))
 		
 		pose[2] = self.pose[2]
 		theta = pose[2]
 		
 		r = math.sqrt((cell_pos[0] - pose[0])**2 + (cell_pos[1] - pose[1])**2)
-		print("r = " + str(r))
 		phi = math.atan2((cell_pos[1] - pose[1]), (cell_pos[0] - pose[0])) - theta
-		print("phi = " + str(phi))
+		
 
 		ang_dif = []
 		ang = np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)
@@ -90,60 +92,79 @@ class grid_map(object):
 
 		k = np.argmin(ang_dif)
 		#print(ang_dif)
-		print("k = " + str(k))
+		#print("r-msg.range[k] = " + str(abs(r-msg.ranges[k])) + "::::::::alpha = " + str(self.alpha/2.0))
+		if abs(r - msg.ranges[k]) < 0:
+			print(cell)
+			print("Pose: " + str(pose))
+			print("Cell position: " + str(cell_pos))
+			print("r = " + str(r))
+			print("phi = " + str(phi))
+			print("k = " + str(k) + ":::::msg.range(k) = " + str(msg.ranges[k]))
+			print("angle(k) = " + str(ang[k]))
 
-		if r > min(self.z_max, msg.ranges[k]) or abs(phi - ang[k]) > self.beta/2:
+		
+
+		if r > min(self.z_max, msg.ranges[k] + self.alpha/2.0) or abs(phi - ang[k]) > self.beta/2.0:
 			#print('Out of range')
 			return self.l0
-		if msg.ranges[k] < self.z_max and abs(r - msg.ranges[k]) < self.alpha/2:
-			print('Cell' + str(cell) + ' is occupied')
+		if msg.ranges[k] < self.z_max and abs(r - msg.ranges[k]) < self.alpha/2.0:
+			#print('Cell' + str(cell) + ' is occupied')
 			return self.l_occ
 		if r <= msg.ranges[k]:
-			print('Cell' + str(cell) + ' is free')
+			#print('Cell' + str(cell) + ' is free')
 			return self.l_free
+		else:
+			print("Error:This cell is neither occupied, free or out of range")
+			return self.l0
 
 	def threshold_map(self):
+		#Inverts from log odds to get the probability in the interval [0,100]
+		#Does not work as intended, 0 in log odds gets mapped to 62, should be 50
 
 		self.thres_map = np.exp(self.grid) * 100 / (np.exp(self.grid) + 1)
 
 	def print_map_to_file(self):
+		'''Prints the grid map of probabilities to a text file
+		Not well implemented yet'''
 		
 		with open('test_map.txt','wb') as f:
     			for line in self.thres_map:
         			np.savetxt(f, line, fmt='%.2f')
 
 	def world_to_grid(self, world):
+		#Transformation from world coordinates to grid_coordinates
 		
-		grid_origin = [self.width / 2, self.height / 2, 0]
+		grid_origin = [self.width * self.res / 2.0, self.height * self.res / 2, 0]
 		return np.add(grid_origin, world)
 
 	def grid_to_world(self, grid):
+		#Transformation from grid coordinates to world coordinates
 		
-		grid_origin = [self.width / 2, self.height / 2]
+		grid_origin = [self.width * self.res / 2.0, self.height * self.res / 2]
 		return grid - grid_origin
 
 	def get_cell_pos(self, index):
-		'''
-		origin is at the center of the grid, cell [0,0] is 
-		at the bottom left corner
+		'''Get the coordinates of the centre of the cell in the grid frame, from the index
+		Origin is at the center of the grid, cell [0,0] is at the top left corner
 		'''
 		pos = [0,0]
-		pos[0] = (-self.width/2.0) + ((index[1] + 1.0) / (4 * self.res))
-		pos[1] = (-self.height/2.0) + ((index[0] + 1.0) / (4 * self.res))
+		pos[0] = (-self.width * self.res /2.0) + ((index[1] + 0.5) * self.res)
+		pos[1] = (self.height * self.res /2.0) - ((index[0] + 0.5) * self.res)
 		return pos
 		
 	def publish_map(self):
+		#Publish map to topic /mbot/occMap
 
 		map_msg = OccupancyGrid()
 		map_msg.header.frame_id = 'map'
-		map_msg.info.resolution = 1.0/self.res
-		map_msg.info.width = self.width * self.res # change later (maybe change width and height to cells instead of metres)
-		map_msg.info.height = self.height * self.res # change later
-		map_msg.data = range(self.width*self.height*self.res**2)
+		map_msg.info.resolution = self.res
+		map_msg.info.width = self.width
+		map_msg.info.height = self.height
+		map_msg.data = range(self.width*self.height)
 
 		# map origin
-		#map_msg.info.origin.position.x = self.width / 2
-		#map_msg.info.origin.position.y = self.height / 2
+		map_msg.info.origin.position.x = - self.width * self.res / 2.0
+		map_msg.info.origin.position.y = - self.height * self.res / 2.0
 		
 		# get current time
 		map_msg.header.stamp = rospy.Time.now()
@@ -151,7 +172,7 @@ class grid_map(object):
 		# might be out of order (should be row major order)
 		self.threshold_map()
 		it = np.nditer(self.thres_map, flags=['multi_index'])
-		for i in range(self.width*self.height*self.res**2):
+		for i in range(self.width*self.height):
 			map_msg.data[i] = self.thres_map[it.multi_index]
 			it.iternext()
 
@@ -244,7 +265,7 @@ def main():
 	map_msg.info.height = height
 	map_msg.data = range(width*height)
 	'''
-	# initialize grid map
+	# create grid map object
 	map = grid_map()
 
 	rospy.init_node('mbot_occ_grid_map', anonymous=False)
