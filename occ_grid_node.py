c27aebaccf582950a81d1beefaededb15f7bfc74
WalkingThyroid <56550454+WalkingThyroid@users.noreply.github.com>
2019-12-20 19:29:56 +0000

    Add files via upload

--- occ_grid_node.py
+++ occ_grid_node.py
@@ -13,25 +13,31 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from nav_msgs.msg import OccupancyGrid
 from tf.transformations import euler_from_quaternion
+from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
 import math
 import numpy as np
+import tf
 
 class grid_map(object):
 
 	def __init__(self):
 
 		self.occ_pub = rospy.Publisher("/mbot/occMap", OccupancyGrid, queue_size = 10)
+
+		# create tf listener
+		self.listener = tf.TransformListener()
 	
-		self.height = 72	# cells
-		self.width = 72		# cells
-		self.res = 0.25		# m/cell
+		self.height = 720		# cells
+		self.width = 720		# cells
+		self.res = 0.025		# m/cell
 		self.pose = [0,0,0]
 		
-		self.alpha = 0.5
+		self.alpha = 0.1
 		self.beta = 5*np.pi/360 # 5 degrees
 		self.z_max = 10  	# initialized on update
+		self.z_min = 0  	# initialized on update
 		
 		self.l_occ = np.log(0.7/0.3)
 		self.l_free = np.log(0.3/0.7)
@@ -56,7 +62,7 @@ class grid_map(object):
 
 		
 	def update_pose(self, msg):
-		'''Updates the robot pose after receiving an odometry message'''
+		"""Updates the robot pose after receiving an odometry message"""
 		
 		self.pose[0] = msg.pose.pose.position.x
 		self.pose[1] = msg.pose.pose.position.y
@@ -68,10 +74,29 @@ class grid_map(object):
 		print("Pose was updated")
 
 	def update(self, msg):
-		'''Update the map after receiving message with measurements'''
+		"""Update the map after receiving message with measurements"""
+
+		worked = 0
+		while(not worked):
+			try:
+				# listen to transform
+				self.listener.waitForTransform('/map', '/base_laser_front_link', rospy.Time(0), rospy.Duration(1.0))
+				(trans,rot) = self.listener.lookupTransform('/map', '/base_laser_front_link', rospy.Time(0))
+				worked = 1
+			except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+				print("Error looking up transform!")
+				worked = 0
+
+		self.pose[0] = trans[0]
+		self.pose[1] = trans[1]
+		(roll, pitch, yaw) = euler_from_quaternion(rot)
+		self.pose[2] = yaw
+
+		print("Pose was updated")
 
 		pose = self.pose
 		self.z_max = msg.range_max
+		self.z_min = msg.range_min
 
 		''' This is the rectangle approach
 		# get the square on which we want to iterate
@@ -114,13 +139,17 @@ class grid_map(object):
 		for i in range(idx_min, idx_max+1):
 			for j in range(idy_min, idy_max+1):
 				self.grid[i,j] += self.inv_model([i,j], msg, pose)
+
+		# avoid overflows
+		self.grid[self.grid > 50] = 50
+		self.grid[self.grid < -50] = -50
 			
 		print("Finished one iteration")
 		self.publish_map()
 		#self.print_map_to_file()
 			
 	def inv_model(self, cell, msg, pose):
-		'''Implementation of inverse model of Probabilistic Robotics'''
+		"""Implementation of inverse model of Probabilistic Robotics"""
 
 		#pose = self.world_to_grid(self.pose)
 		#cell_pos = self.get_cell_pos(cell)
@@ -135,12 +164,20 @@ class grid_map(object):
 		phi = math.atan2((cell_pos[1] - pose[1]), (cell_pos[0] - pose[0])) - theta
 		
 
-		ang_dif = []
 		ang = np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)
-
+		'''
 		for i in range(0,len(ang)):
 			#ang_dif.append(math.atan2( math.sin(phi - ang[i]), math.cos(phi - ang[i]))) # maybe?
 			ang_dif.append(abs( phi - ang[i])) # not sure about this, actually
+		'''
+
+		ang_dif = np.full((1, ang.size), phi) - ang
+
+		# wrap to [-pi, pi]
+		ang_dif[ang_dif > np.pi] -= 2.0 * np.pi
+		ang_dif[ang_dif < -np.pi] += 2.0 * np.pi
+
+		ang_dif = np.abs(ang_dif)
 
 		k = np.argmin(ang_dif)
 		#print(ang_dif)
@@ -154,9 +191,11 @@ class grid_map(object):
 			print("k = " + str(k) + ":::::msg.range(k) = " + str(msg.ranges[k]))
 			print("angle(k) = " + str(ang[k]))
 
-		
+		# discard error measurements
+		if msg.ranges[k] > self.z_max or msg.ranges[k] < self.z_min:
+			return self.l0
 
-		if r > min(self.z_max, msg.ranges[k] + self.alpha/2.0) or abs(phi - ang[k]) > self.beta/2.0:
+		if r > min(self.z_max, msg.ranges[k] + self.alpha/2.0) or abs(ang_dif[0,k]) > self.beta/2.0:
 			#print('Out of range')
 			return self.l0
 		if msg.ranges[k] < self.z_max and abs(r - msg.ranges[k]) < self.alpha/2.0:
@@ -169,20 +208,20 @@ class grid_map(object):
 			#print("Error:This cell is neither occupied, free or out of range")
 			return self.l0
 
-	def threshold_map(self):
+	def inv_log_odds_map(self):
 		#Inverts from log odds to get the probability in the interval [0,100]
 		#Does not work as intended, 0 in log odds gets mapped to 62, should be 50
 
 		odds = np.exp(self.grid)
-		self.thres_map = odds * 100 / (odds + 1)
+		self.norm_map = odds * 100 / (odds + 1)
 
 	def print_map_to_file(self):
-		'''Prints the grid map of probabilities to a text file
-		Not well implemented yet'''
+		"""Prints the grid map of probabilities to a text file
+		Not well implemented yet"""
 		
 		with open('test_map.txt','wb') as f:
-    			for line in self.thres_map:
-        			np.savetxt(f, line, fmt='%.2f')
+			for line in self.norm_map:
+				np.savetxt(f, line, fmt='%.2f')
 
 	def world_to_grid(self, world):
 		#Transformation from world coordinates to grid_coordinates
@@ -197,9 +236,9 @@ class grid_map(object):
 		return grid - grid_origin
 
 	def get_cell_pos(self, index):
-		'''Get the coordinates of the centre of the cell in the grid frame, from the index
+		"""Get the coordinates of the centre of the cell in the grid frame, from the index
 		Origin is at the center of the grid, cell [0,0] is at the top left corner
-		'''
+		"""
 		pos = [0,0]
 		pos[0] = (-self.width * self.res /2.0) + ((index[1] + 0.5) * self.res)
 		pos[1] = (self.height * self.res /2.0) - ((index[0] + 0.5) * self.res)
@@ -217,32 +256,43 @@ class grid_map(object):
 
 		# map origin
 		map_msg.info.origin.position.x = - self.width * self.res / 2.0
-		map_msg.info.origin.position.y = - self.height * self.res / 2.0
+		map_msg.info.origin.position.y = self.height * self.res / 2.0
+		[x,y,z,w] = quaternion_from_euler(0,0,-np.pi/2)
+		map_msg.info.origin.orientation.x = x
+		map_msg.info.origin.orientation.y = y
+		map_msg.info.origin.orientation.z = z
+		map_msg.info.origin.orientation.w = w
 		
 		# get current time
 		map_msg.header.stamp = rospy.Time.now()
 
 		# might be out of order (should be row major order)
-		self.threshold_map()
-		it = np.nditer(self.thres_map, flags=['multi_index'])
+		self.inv_log_odds_map()
+		self.threshold_map() # just a test
+		it = np.nditer(self.norm_map, flags=['multi_index'])
 		for i in range(self.width*self.height):
-			map_msg.data[i] = self.thres_map[it.multi_index]
+			map_msg.data[i] = self.norm_map[it.multi_index]
 			it.iternext()
 
 		self.occ_pub.publish(map_msg)
 
 	def laser_callback(self, msg):
-		''' Save the laser message in a list '''
+		""" Save the laser message in a list """
 		if len(self.laser_msgs) > 9:
 			self.laser_msgs = self.laser_msgs[1:]
 
 		self.laser_msgs.append(msg)
 	
 	def pose_callback(self, msg):
-		''' Save the pose message in a list '''
+		""" Save the pose message in a list """
 		if len(self.pose_msgs) > 9:
 			self.pose_msgs = self.pose_msgs[1:]
 		self.pose_msgs.append(msg)
+
+	def threshold_map(self):
+		self.norm_map[self.norm_map > 50] = 100
+		self.norm_map[self.norm_map < 50] = 0
+		self.norm_map[self.norm_map == 50] = -1
 
 
 
@@ -261,10 +311,10 @@ class behaviour(object):
 		
 		
 	def laserCallback(self, msg):
-		'''
+		"""
 		This function gets executed everytime a laser scanner msg is received on the
 		topic: /robot_0/base_scan_1
-		'''
+		"""
 		# ============= YOUR CODE GOES HERE! =====
 		# hint: msg contains the laser scanner msg
 		# hint: check http://docs.ros.org/api/sensor_msgs/html/msg/LaserScan.html
@@ -333,22 +383,27 @@ def main():
 	map_msg.info.height = height
 	map_msg.data = range(width*height)
 	'''
+
+	# initialize nodes
+	rospy.init_node('mbot_occ_grid_map', anonymous=False)
+
+	# print message in terminal
+	rospy.loginfo('Mbot Occupancy Grid Mapping started !')
+
 	# create grid map object
 	map = grid_map()
 
-	rospy.init_node('mbot_occ_grid_map', anonymous=False)
-	# print message in terminal
-	rospy.loginfo('Mbot Occupancy Grid Mapping started !')
-	# subscibe to the two laser range finders
-	rospy.Subscriber('/robot_0/base_scan_1', LaserScan, map.laser_callback, queue_size=1)
-	#rospy.Subscriber('/scan_front', LaserScan, map.laser_callback, queue_size=None)
+	# subscribe to the two laser range finders
+	#rospy.Subscriber('/robot_0/base_scan_1', LaserScan, map.laser_callback, queue_size=1)
+	rospy.Subscriber('/scan_front', LaserScan, map.laser_callback, queue_size=None)
 	# rospy.Subscriber('/scan2')
-	rospy.Subscriber('/robot_0/base_pose_ground_truth', Odometry, map.pose_callback)
+	#rospy.Subscriber('/robot_0/base_pose_ground_truth', Odometry, map.pose_callback)
 	#rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, map.pose_callback)
 
 	while not rospy.is_shutdown():
-		if map.pose_msgs and map.laser_msgs:
-			map.update_pose(map.pose_msgs[-1])
+
+		if map.laser_msgs:
+			#map.update_pose(map.pose_msgs[-1])
 			map.update(map.laser_msgs[-1])
 
 		
